1f95b1f8f52e67c1c28db43e78684c5f6237ded4
widigdacahya <rizkiwidigda@gmail.com>
2022-05-16 23:31:55 +0700

    app run but respones failure

--- 1_api/app/src/main/java/com/cahyadesthian/apichatbot/ChatRVAdapter.kt
+++ 1_api/app/src/main/java/com/cahyadesthian/apichatbot/ChatRVAdapter.kt
@@ -1,4 +1,87 @@
 package com.cahyadesthian.apichatbot
 
-class ChatRVAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.cahyadesthian.apichatbot.databinding.BotMsgRvItemBinding
+import com.cahyadesthian.apichatbot.databinding.UserMsgRvItemBinding
+import com.cahyadesthian.apichatbot.model.ChatModel
+import java.nio.file.Files.find
+
+
+class ChatRVAdapter(context: Context, listMessage: ArrayList<ChatModel>) :  RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    companion object {
+        const val BOT_MESSAGE_VIEW = 0
+        const val USER_MESSAGE_VIEW = 1
+    }
+
+    //private val ourContext: Context = context
+    var theMessageList: ArrayList<ChatModel> = listMessage
+
+    private inner class BotMessageItemViewHolder(val itemBotMsgBinding: BotMsgRvItemBinding): RecyclerView.ViewHolder(itemBotMsgBinding.root) {
+
+        fun bindBotMsg(position: Int) {
+            val recyclerViewModel = theMessageList[position]
+            itemBotMsgBinding.tvBot.text = recyclerViewModel.message
+        }
+
+    }
+
+    private inner class UserMessageItemViewHolder(val itemUserMsgBinding: UserMsgRvItemBinding): RecyclerView.ViewHolder(itemUserMsgBinding.root) {
+
+        fun bindUserMsg(position: Int) {
+            val recyclerViewModel = theMessageList[position]
+            itemUserMsgBinding.tvUser.text = recyclerViewModel.message
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        when(viewType) {
+//            0 -> return BotMessageItemViewHolder(BotMsgRvItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+//            1 -> return UserMessageItemViewHolder(UserMsgRvItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+//        }
+        return if(viewType == 0) {
+            BotMessageItemViewHolder(BotMsgRvItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        } else {
+            UserMessageItemViewHolder(UserMsgRvItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        //val chatsModal = theMessageList[position]
+//        when(chatsModal.sender) {
+//            "bot" -> BotMessageItemViewHolder.bindBo
+//        }
+        if(theMessageList[position].sender == "bot") {
+            (holder as BotMessageItemViewHolder).bindBotMsg(position)
+        } else {
+            (holder as UserMessageItemViewHolder).bindUserMsg(position)
+        }
+    }
+
+    override fun getItemCount(): Int = theMessageList.size
+
+    override fun getItemViewType(position: Int): Int {
+//        return if(theMessageList.get(position).sender == "bot") {
+//            return 0
+//        } else {
+//            return 1
+//        }
+//        when(theMessageList[position].sender) {
+//            "bot" -> return 0
+//            "user" -> return 1
+//
+//        }
+        return if(theMessageList[position].sender== "bot") {
+            0
+        } else {
+            1
+        }
+    }
+
+
 }
--- 1_api/app/src/main/java/com/cahyadesthian/apichatbot/MainActivity.kt
+++ 1_api/app/src/main/java/com/cahyadesthian/apichatbot/MainActivity.kt
@@ -2,10 +2,89 @@ package com.cahyadesthian.apichatbot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cahyadesthian.apichatbot.databinding.ActivityMainBinding
+import com.cahyadesthian.apichatbot.model.BotResponse
+import com.cahyadesthian.apichatbot.model.ChatModel
+import com.cahyadesthian.apichatbot.networking.ApiService
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private var _mainBinding : ActivityMainBinding? = null
+    private val mainBinding get() = _mainBinding!!
+
+    private lateinit var chatList : ArrayList<ChatModel>
+
+    companion object {
+        const val BOT_MESSAGE_KEY= "bot"
+        const val USER_MESSAGE_KEY= "user"
+    }
+
+    private lateinit var adapter: ChatRVAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        _mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        chatList = arrayListOf()
+
+        adapter = ChatRVAdapter(this,chatList)
+        mainBinding.rvMessageArea.adapter = adapter
+        mainBinding.rvMessageArea.layoutManager = LinearLayoutManager(applicationContext)
+
+
+        mainBinding.btnSend.setOnClickListener {
+            if(mainBinding.etMessage.text.isNotEmpty()) {
+                getResponse(mainBinding.etMessage.text.toString())
+                mainBinding.etMessage.setText("")
+            }
+        }
+
     }
+
+    private fun getResponse(message: String) {
+        chatList.add(ChatModel(message,USER_MESSAGE_KEY))
+        adapter.notifyDataSetChanged()
+        val url = "http://api.brainshop.ai/get?bid=166207&key=We1Dz5ZfDzZWHvDU&uid=[uid]&msg=$message"
+        val BASE_URL = "http://api.brainshop.ai/"
+        val retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        val retrofitApi = retrofit.create(ApiService::class.java)
+//        val call : Call<BotResponse> = retrofitApi.getBotResponse(url)
+//
+//        call.enqueue(object Callback<BotResponse> {
+//
+//        })
+        retrofitApi.getBotResponse(url).enqueue(object : Callback<BotResponse>{
+            override fun onResponse(call: Call<BotResponse>, response: Response<BotResponse>) {
+                if(response.isSuccessful) {
+                    val modal : BotResponse? = response.body()
+                    modal?.let { ChatModel(it.cnt, BOT_MESSAGE_KEY) }?.let { chatList.add(it) }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onFailure(call: Call<BotResponse>, t: Throwable) {
+                chatList.add(ChatModel("Revert Question please", BOT_MESSAGE_KEY))
+                adapter.notifyDataSetChanged()
+            }
+
+        })
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mainBinding = null
+    }
+
 }
